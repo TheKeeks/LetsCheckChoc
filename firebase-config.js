b6e550a5b8812fcca79331b426ffd1361d7ebe37
TheKeeks <121732575+TheKeeks@users.noreply.github.com>
2026-02-26 15:19:18 -0500

    Update firebase-config.js with actual Firebase credentials

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,10 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyBWuLMPKGS91HSOzmQALinQl3w5FwkIdIs",
-  authDomain: "letscheckchoc.firebaseapp.com",
-  projectId: "letscheckchoc",
-  storageBucket: "letscheckchoc.firebasestorage.app",
-  messagingSenderId: "801516961544",
-  appId: "1:801516961544:web:c28584674e5be4643e36bd",
-  measurementId: "G-JFYS74CC6D"
+    apiKey: "AIzaSyBWuLMPKGS91HSOzmQALinQl3w5FwkIdIs",
+    authDomain: "letscheckchoc.firebaseapp.com",
+    projectId: "letscheckchoc",
+    storageBucket: "letscheckchoc.firebasestorage.app",
+    messagingSenderId: "801516961544",
+    appId: "1:801516961544:web:c28584674e5be4643e36bd"
 };
 
 export default firebaseConfig;
